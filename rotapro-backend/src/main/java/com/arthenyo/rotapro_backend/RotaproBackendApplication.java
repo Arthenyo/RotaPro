624c45c77aae41c4ce03b18579651308da542025
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-12-04 16:36:04 -0300

    feat(backend): inicialização do projeto backend com Spring Boot

---
+++ rotapro-backend/src/main/java/com/arthenyo/rotapro_backend/RotaproBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arthenyo.rotapro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaproBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaproBackendApplication.class, args);
+	}
+
+}
---
+++ rotapro-backend/src/test/java/com/arthenyo/rotapro_backend/RotaproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthenyo.rotapro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
