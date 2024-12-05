8b184f12bd60c0bdefead91584a3205319286903
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-12-05 08:31:40 -0300

    feat(backend): adiciona configuração para bancos de dados Oracle e PostgreSQL

---
+++ rotapro-backend/src/main/java/com/arthenyo/rotapro_backend/config/OracleDbConfig.java
@@ -0,0 +1,50 @@
+package com.arthenyo.rotapro_backend.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.arthenyo.rotapro.repository.oracle",
+        entityManagerFactoryRef = "oracleEntityManagerFactory",
+        transactionManagerRef = "transactionManagerOracle"
+)
+@ConditionalOnProperty(name = "oracle.datasource.jdbc-url")
+public class OracleDbConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "oracle.datasource")
+    public DataSource oracleDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "oracleEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean oracleEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("oracleDataSource") DataSource dataSource
+    ) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.arthenyo.rotapro.model.oracle")
+                .persistenceUnit("oracle")
+                .build();
+    }
+
+    @Bean(name = "transactionManagerOracle")
+    public PlatformTransactionManager transactionManagerOracle(
+            @Qualifier("oracleEntityManagerFactory") LocalContainerEntityManagerFactoryBean oracleEntityManagerFactoryBean
+    ) {
+        return new JpaTransactionManager(oracleEntityManagerFactoryBean.getObject());
+    }
+}
---
+++ rotapro-backend/src/main/java/com/arthenyo/rotapro_backend/config/PostgresqlDbConfig.java
@@ -0,0 +1,50 @@
+package com.arthenyo.rotapro_backend.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.arthenyo.rotapro.repository.postgresql",
+        entityManagerFactoryRef = "postgresqlEntityManager"
+)
+@ConditionalOnProperty(name = "postgresql.datasource.jdbc-url")
+public class PostgresqlDbConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "postgresql.datasource")
+    public DataSource postgresqlDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean postgresqlEntityManager(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("postgresqlDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.arthenyo.rotapro.model.postgresql")
+                .build();
+    }
+    @Bean(name = "transactionManager")
+    @Primary
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("postgresqlEntityManager") LocalContainerEntityManagerFactoryBean postgresqlEntityManagerFactoryBean) {
+        return new JpaTransactionManager(postgresqlEntityManagerFactoryBean.getObject());
+    }
+
+
+}
